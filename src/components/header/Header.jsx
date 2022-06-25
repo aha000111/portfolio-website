becaa83ce545fb80b126e5df45bd1e718d31c4f2
Amir <aha000111@icloud.com>
2022-06-25 13:14:16 +0100

    added CV-downloadable button to Header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
-
+import CTA from './CTA';
 import './header.css';
+import ME from '../../assets/me.jpeg';
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Amir Ali</h1>
+        <h5 className="text-light">Fullstack Developer</h5>
+        <CTA />
+
+
+        <div className="me">
+          <img src={ME} alt="me" />
+        </div>
+
+        <a href="#contact" className='scroll_down'>Scroll Down</a>
+      </div>
+    </header>
   )
 }
 
