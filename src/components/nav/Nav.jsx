bd12f9f7d6fb5dadf8b11657bf8d4b4d800b4d81
Amir <aha000111@icloud.com>
2022-06-25 15:31:42 +0100

    add navbar icons for each section

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import './nav.css';
-import {FaHome} from 'react-icons/fa'
+import {FaHome} from 'react-icons/fa';
+import {BsFilePerson} from 'react-icons/bs';
+import {FiBook} from 'react-icons/fi';
+import {RiServiceFill} from 'react-icons/ri';
+import {RiMessage2Line} from 'react-icons/ri'
+
+
 
 const nav = () => {
   return (
     <nav>
       <a href="#"><FaHome /></a>
+      <a href="about"><BsFilePerson /></a>
+      <a href="experience"><FiBook /></a>
+      <a href="services"><RiServiceFill /></a>
+      <a href="contact"><RiMessage2Line /></a>
+
     </nav>
   )
 }
