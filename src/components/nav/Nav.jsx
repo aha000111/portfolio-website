3291714b1486a787f5ceffe8b174fbfea8328cfd
Amir <aha000111@icloud.com>
2022-06-25 16:10:19 +0100

    useState hooks for icons active classes and fixed nav error

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -4,21 +4,23 @@ import {FaHome} from 'react-icons/fa';
 import {BsFilePerson} from 'react-icons/bs';
 import {FiBook} from 'react-icons/fi';
 import {RiServiceFill} from 'react-icons/ri';
-import {RiMessage2Line} from 'react-icons/ri'
+import {RiMessage2Line} from 'react-icons/ri';
+import { useState } from 'react';
 
 
 
-const nav = () => {
+const Nav = () => {
+  const [activeNav, setActiveNav] = useState('#');
   return (
     <nav>
-      <a href="#" className='active'><FaHome /></a>
-      <a href="#about"><BsFilePerson /></a>
-      <a href="#experience"><FiBook /></a>
-      <a href="#services"><RiServiceFill /></a>
-      <a href="#contact"><RiMessage2Line /></a>
+      <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><FaHome /></a>
+      <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><BsFilePerson /></a>
+      <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><FiBook /></a>
+      <a href="#services" onClick={() => setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''}><RiServiceFill /></a>
+      <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><RiMessage2Line /></a>
 
     </nav>
   )
 }
 
-export default nav
+export default Nav
