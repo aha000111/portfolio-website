c9c30b320da825dd34816d5d0ab7da98cab5a252
Amir <aha000111@icloud.com>
2022-07-01 00:58:53 +0100

    add site under construction to Header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -8,8 +8,8 @@ const Header = () => {
   return (
     <header>
       <div className="container header__container">
-        <h5>Hello I'm</h5>
-        <h1>Amir Ali</h1>
+        <h5>Hello I'm Amir Ali</h5>
+        <h1>SITE UNDER CONSTRUCTION</h1>
         <h5 className="text-light">Fullstack Developer</h5>
         <CTA />
         <HeaderSocials />
