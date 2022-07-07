d04958cfc21234d474b2dbb96070b7f6b7276e45
Amir <aha000111@icloud.com>
2022-07-07 20:24:39 +0100

    add GithubTracker Screenshot and create the foundation inside the portfolio container

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
 import './portfolio.css';
+import IMG1 from '../../assets/GithubTracker.png';
+import IMG2 from '../../assets/MusicApp.png';
+import IMG3 from '../../assets/GithubTracker.png';
+import IMG4 from '../../assets/SmallTalk.png';
+import IMG5 from '../../assets/WhatsmyDoodle.png';
+// import '../../assets/GithubTracker.png';
+
 
 
 const Portfolio = () => {
@@ -10,10 +17,52 @@ const Portfolio = () => {
 
       <div className="container portfolio__container">
         <article className='portfolio__item'>
-          <div className="portfolio__item-image"></div>
+          <div className="portfolio__item-image">
+            <img src={IMG1} alt="screenshot of GitHub Tacker Project" />
+          </div>
           <h3>This is a porfolio item title</h3>
-          <a href="github link" className='btn' target='_blank'>Github</a>
-          <a href="github link" className='btn btn-primary' target='_blank'>Live Demo</a>
+          <a href="https://github.com/aha000111/Github-Tracker-App" className='btn' target='_blank'>Github</a>
+          <a href="https://earnest-stroopwafel-d0b890.netlify.app/" className='btn btn-primary' target='_blank'>Live Demo</a>
+        </article>
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src={IMG1} alt="screenshot of GitHub Tacker Project" />
+          </div>
+          <h3>This is a porfolio item title</h3>
+          <a href="https://github.com/aha000111/Github-Tracker-App" className='btn' target='_blank'>Github</a>
+          <a href="https://earnest-stroopwafel-d0b890.netlify.app/" className='btn btn-primary' target='_blank'>Live Demo</a>
+        </article>
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src={IMG1} alt="screenshot of GitHub Tacker Project" />
+          </div>
+          <h3>This is a porfolio item title</h3>
+          <a href="https://github.com/aha000111/Github-Tracker-App" className='btn' target='_blank'>Github</a>
+          <a href="https://earnest-stroopwafel-d0b890.netlify.app/" className='btn btn-primary' target='_blank'>Live Demo</a>
+        </article>
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src={IMG1} alt="screenshot of GitHub Tacker Project" />
+          </div>
+          <h3>This is a porfolio item title</h3>
+          <a href="https://github.com/aha000111/Github-Tracker-App" className='btn' target='_blank'>Github</a>
+          <a href="https://earnest-stroopwafel-d0b890.netlify.app/" className='btn btn-primary' target='_blank'>Live Demo</a>
+        </article>
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src={IMG1} alt="screenshot of GitHub Tacker Project" />
+          </div>
+          <h3>This is a porfolio item title</h3>
+          <a href="https://github.com/aha000111/Github-Tracker-App" className='btn' target='_blank'>Github</a>
+          <a href="https://earnest-stroopwafel-d0b890.netlify.app/" className='btn btn-primary' target='_blank'>Live Demo</a>
+        </article>
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src={IMG1} alt="screenshot of GitHub Tacker Project" />
+          </div>
+          <h3>This is a porfolio item title</h3>
+          <a href="https://github.com/aha000111/Github-Tracker-App" className='btn' target='_blank'>Github</a>
+          <a href="https://earnest-stroopwafel-d0b890.netlify.app/" className='btn btn-primary' target='_blank'>Live Demo</a>
         </article>
       </div>
       
