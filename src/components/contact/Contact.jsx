3d596db814172fce06e5938ee7bd41641d501f70
Amir <aha000111@icloud.com>
2022-07-08 00:57:02 +0100

    add contact information

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,45 @@
 import React from 'react';
 import './contact.css';
+import {MdOutlineMarkEmailUnread} from 'react-icons/md';
+import {BsMessenger} from 'react-icons/bs';
+import {BsWhatsapp} from 'react-icons/bs';
 
 const Contact = () => {
   return (
-    <section id='contact'>Contact</section>
+    <section id='contact'>
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <MdOutlineMarkEmailUnread />
+            <h4>Email</h4>
+            <h5>aha000111@icloud.com</h5>
+            <a href='mailto:aha000111@icloud.com' target="_blank">Send a Message</a>
+          </article>       
+          <article className="contact__option">
+            <BsMessenger />
+            <h4>Messenger</h4>
+            {/* <h5>afsafaf</h5> */}
+            <a href='https://m.me/profile.php?id=100048549632775' target="_blank">Send a Message</a>
+          </article>       
+          <article className="contact__option">
+            <BsWhatsapp />
+            <h4>WhatsApp</h4>
+            <h5>+44 7877332512</h5>
+            <a href="https://api.whatsapp.com/send?phone+447877332512" target="_blank">Send a Message</a>
+          </article>
+        </div>
+        {/* END OF CONTACTS  */}
+        <form action="">
+          <input type="text" name='name' placeholder='Your Full Name' required />
+          <input type="email" name='email' placeholder='Your Email' required />
+          <textarea name="message" rows="7" placeholder='Your Message' required></textarea>
+          <button type='submit' className='btn btn-primary'>Send Message</button>
+        </form>
+      </div>
+    </section>
   )
 }
 
