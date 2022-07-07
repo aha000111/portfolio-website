db41f9b4b25e8c772e262c8a27f7c179d6585b2a
Amir <aha000111@icloud.com>
2022-07-07 14:50:19 +0100

    create portfolio card template and add project screenshots

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
 import './portfolio.css';
 
+
 const Portfolio = () => {
   return (
-    <section id='portfolio'>Portfolio</section>
+    <section id='portfolio'>
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className="container portfolio__container">
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image"></div>
+          <h3>This is a porfolio item title</h3>
+          <a href="github link" className='btn' target='_blank'>Github</a>
+          <a href="github link" className='btn btn-primary' target='_blank'>Live Demo</a>
+        </article>
+      </div>
+      
+      </section>
   )
 }
 
