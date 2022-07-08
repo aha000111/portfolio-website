f8acccb4d70b09d04268daeec39addb4108dbeac
Amir <aha000111@icloud.com>
2022-07-08 01:12:04 +0100

    give icons class name to be able to target them

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -13,19 +13,19 @@ const Contact = () => {
       <div className="container contact__container">
         <div className="contact__options">
           <article className="contact__option">
-            <MdOutlineMarkEmailUnread />
+            <MdOutlineMarkEmailUnread className='contact__option-icon'/>
             <h4>Email</h4>
             <h5>aha000111@icloud.com</h5>
             <a href='mailto:aha000111@icloud.com' target="_blank">Send a Message</a>
           </article>       
           <article className="contact__option">
-            <BsMessenger />
+            <BsMessenger className='contact__option-icon'/>
             <h4>Messenger</h4>
             {/* <h5>afsafaf</h5> */}
             <a href='https://m.me/profile.php?id=100048549632775' target="_blank">Send a Message</a>
           </article>       
           <article className="contact__option">
-            <BsWhatsapp />
+            <BsWhatsapp className='contact__option-icon'/>
             <h4>WhatsApp</h4>
             <h5>+44 7877332512</h5>
             <a href="https://api.whatsapp.com/send?phone+447877332512" target="_blank">Send a Message</a>
