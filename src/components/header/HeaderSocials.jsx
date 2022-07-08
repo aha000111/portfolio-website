a7b46a980e24faaaca93925312411eb8d6ee6fcc
Amir <aha000111@icloud.com>
2022-07-08 02:44:51 +0100

    update header socials icons with correct links

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -5,8 +5,8 @@ import {FaGithub} from 'react-icons/fa';
 const HeaderSocials = () => {
   return (
     <div className="header__socials">
-        <a href="https://linkedin.com" target="_blank"><FaLinkedin/></a>
-        <a href="https://github.com" target="_blank"><FaGithub/></a>
+        <a href="https://linkedin.com/in/amir-ali-cl" target="_blank"><FaLinkedin/></a>
+        <a href="https://github.com/aha000111" target="_blank"><FaGithub/></a>
         <a href="https://dribbble.com" target="_blank"></a>
     </div>
   )
